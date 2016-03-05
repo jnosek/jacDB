6ff94f91e52a106b37f1cb4654c98d96274a6ed3
jnosek <jnosek@gmail.com>
2016-03-05 09:03:43 -0500

    Base project structure

---
+++ jacDB.Core/TableHeader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jacDB.Core
+{
+    class TableHeader
+    {
+        public string Name { get; set; }
+
+        public long Count { get; set; }
+
+        public long LastRowId { get; set; }
+
+        public long LastRowVersion { get; set; }
+    }
+}
