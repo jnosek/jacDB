03f44b26a5996327487c861efac44b65b3c839f9
Jack Nosek <jack.nosek@barcoding.com>
2019-04-13 09:34:01 -0400

    Replaced original projects with .net core and .net standard

---
+++ jacDB.Client.Repl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jacDB.Client.Repl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
